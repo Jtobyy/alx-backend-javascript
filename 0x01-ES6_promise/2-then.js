12a2726d3194007a5675260a2b0e3e81b73e99e0
Jtobyy <jtobi8161@gmail.com>
2022-04-08 13:42:36 +0100

    2. Catch me if you can!

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,4 @@
+export default function handleResponseFromAPI(promise) {
+  promise.then(() => ({ status: 200, body: 'success' }), () => (Error()))
+    .finally(console.log('Got a response from the API'));
+}
