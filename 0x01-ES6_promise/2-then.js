213892cacdf002d62a5aaa22f4985955082ae81b
Jtobyy <jtobi8161@gmail.com>
2022-04-08 13:46:02 +0100

    2. Catch me if you can./0-main.js  Use .catch handler

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,4 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({ status: 200, body: 'success' }), () => (Error()))
+  promise.then(() => ({ status: 200, body: 'success' }))
+    .catch(() => (Error()))
     .finally(console.log('Got a response from the API'));
 }
