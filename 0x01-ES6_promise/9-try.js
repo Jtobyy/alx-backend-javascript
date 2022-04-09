0fe788dc35f7a371ddce0ff8326eea179b4032d4
Jtobyy <jtobi8161@gmail.com>
2022-04-09 16:36:29 +0100

    9. Throw an error

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -4,7 +4,7 @@ export default function guardrail(mathFunction) {
     const value = mathFunction();
     queue.push(value);
   } catch (e) {
-    queue.push(e.message);
+    queue.push(`Error: ${e.message}`);
   } finally {
     queue.push('Guardrail was processed');
   }
