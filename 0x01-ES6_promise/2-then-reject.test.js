9e148779f6abc42357f75e0781c636168f174df6
Jtobyy <jtobi8161@gmail.com>
2022-04-09 15:25:48 +0100

    Finally fixed 6-final-user.js using extra properties of a returned promise e.g status, reason etc

--- 0x01-ES6_promise/2-then-reject.test.js
+++
@@ -1,8 +0,0 @@
-import handleResponseFromAPI from "./2-then";
-
-test("handleResponseFromAPI return the right response on reject", () => {
-    const promise = Promise.reject(new Error());
-
-    const successResponse = handleResponseFromAPI(promise);
-    return expect(successResponse).resolves.toEqual(new Error());
-});
--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,13 +2,19 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const myArr = [];
-  function myFunc(func, ...args) {
-    return func(args)
-      .then((obj) => ({ status: 'resolved', value: obj }))
-      .catch((obj) => ({ status: 'rejected', value: obj }));
-  }
-  myArr.push(myFunc(signUpUser, firstName, lastName));
-  myArr.push(myFunc(uploadPhoto, fileName));
-  return myArr;
+  const myArray = [];
+  let myObj = {};
+  const myPromise = Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((objArr) => {
+      objArr.forEach((obj) => {
+        if (obj.status === 'rejected') {
+          myObj = { status: obj.status, value: `Error: ${obj.reason.message}` };
+        } else {
+          myObj = { status: obj.status, value: obj.value };
+        }
+        myArray.push(myObj);
+      });
+      return myArray;
+    });
+  return myPromise;
 }
