776aff9e0f6045f8cee9f808343b73f91611fc17
Jtobyy <jtobi8161@gmail.com>
2022-04-10 19:27:08 +0100

    8. Primitive - Holberton Class

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -2,7 +2,7 @@ export default class HolbertonClass {
   constructor(size, location) {
     this._size = size;
     this._location = location;
-    Number = (thisInstance) => (thisInstance._size);
-    String = (thisInstance) => (thisInstance._location);
+    globalThis.Number = (thisInstance) => (thisInstance._size);
+    globalThis.String = (thisInstance) => (thisInstance._location);
   }
 }
