85db301793671b323898f4e50d26ef0ef5c3f043
Jtobyy <jtobi8161@gmail.com>
2022-04-15 23:44:56 +0100

    Fix test cases

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,18 +1,20 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') {
+  if (startString === '' || startString === undefined || typeof startString !== 'string') {
     return '';
   }
   let myString = '';
   const spliceLength = startString.length;
   set.forEach((el) => {
-    if (el.startsWith(startString) === true) {
+    if (el === undefined) {
+      myString += '';
+    } else if (el.startsWith(startString) === true) {
       myString += `${[...el].splice(spliceLength).join('')}-`;
     }
   });
   const stringArray = [...myString];
-  stringArray.pop();
   while (stringArray[0] === '-') {
     stringArray.shift();
   }
+  stringArray.pop();
   return stringArray.join('');
 }
